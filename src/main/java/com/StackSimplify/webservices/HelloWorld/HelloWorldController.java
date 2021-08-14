a7ea4ee6eff651dc9bbc3af1ba2c961ee9993463
unknown <gvt1243@gmail.com>
2021-08-14 05:42:47 -0700

    Hello world

---
+++ src/main/java/com/StackSimplify/webservices/HelloWorld/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.StackSimplify.webservices.HelloWorld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping(method=RequestMethod.GET, path="/hello")
+	public String hello()
+	{
+		return "Hi Vishal" ;
+	}
+}
