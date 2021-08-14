00b4de0c2dc38d38c7e31db2138fb35e570da134
admin <gvt1243@gmail.com>
2021-08-14 02:46:46 -0700

    rd

--- src/main/java/com/StackSimplify/webservices/SpringbootBuildingblockApplication.java
+++ src/main/java/com/StackSimplify/webservices/SpringbootBuildingblockApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootBuildingblockApplication {
 
-	//test tes
+	//test tesf
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblockApplication.class, args);
 	}
