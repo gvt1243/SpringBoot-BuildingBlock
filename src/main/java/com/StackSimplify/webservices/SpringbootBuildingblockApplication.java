3675480f422120fd81c7f1eafdedc77e7ff4bd6f
admin <gvt1243@gmail.com>
2021-08-14 03:31:58 -0700

    Hida

--- src/main/java/com/StackSimplify/webservices/SpringbootBuildingblockApplication.java
+++ src/main/java/com/StackSimplify/webservices/SpringbootBuildingblockApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootBuildingblockApplication {
 
-	//test tesf 
+	//test tesfs
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblockApplication.class, args);
 	}
